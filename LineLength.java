5b59e0c87cd2ae05137cf6c84ebc7d853b76944e
Jay22031999 <Jay22031999>
2021-07-25 14:24:40 +0530

    assignment-01

---
+++ LineLength.java
@@ -0,0 +1,10 @@
+public class LineLength{
+  public static void main(String[] args){
+  int x1 = 4;
+  int x2 = 8;
+  int y1 = 3;
+  int y2 = 6;
+double length = Math.sqrt(((x2-x1)*(x2-x1))+((y2-y1)*(y2-y1)));
+  System.out.println(length);
+  }
+}
